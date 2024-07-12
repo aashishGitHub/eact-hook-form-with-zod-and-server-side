eaf32399249acebf6299e3f6f4994bb18172b11f
aashishkrCB <aashish123pqr@gmail.com>
2024-07-12 12:55:45 +0530

    added GitIgnore

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import FormWithReactHookForm from "@/components/form-with-react-hook-form";
+import FormWithReactHookFormAndZod from "@/components/form-with-rhf-and-zod";
+import FormWithReactHookFormAndZodAndServer from "@/components/form-with-rhf-and-zod-and-server";
+import FormWithoutReactHookForm from "@/components/form-without-react-hook-form";
+import WithReactHookDynamicForm from "@/components/with-react-hook-dynamic-form";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <WithReactHookDynamicForm />
+      {/* <FormWithoutReactHookForm /> */}
+      {/* <FormWithReactHookForm /> */}
+      {/* <FormWithReactHookFormAndZod /> */}
+      {/* <FormWithReactHookFormAndZodAndServer /> */}
+    </main>
+  );
+}
